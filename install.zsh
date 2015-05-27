1982880773b3d1ae0e5e1786a98cac78a8bc1979
Jannis R <mail@jannisr.de>
2015-05-27 21:45:04 +0200

    removed  for better performance

--- install.zsh
+++ install.zsh
@@ -7,7 +7,6 @@ ln -s	~/.config/editorconfig/editorconfig	~/.editorconfig
 ln -s	~/.config/git/gitignore				~/.gitignore
 ln -s	~/.config/git/gitconfig				~/.gitconfig
 ln -s	~/.config/npm/npmrc					~/.npmrc
-ln -s	~/.config/git/irbrc					~/.irbrc
 ln -s	~/.config/zsh/zshrc					~/.zshrc
 ln -s	~/.config/bash/bash_profile			~/.bash_profile
 touch	~/.hushlogin

aac1e1c14ea40dd2606f51e1f25c9ae20c5711ec
Jannis R <mail@jannisr.de>
2015-06-16 00:12:19 +0200

    added my homebrew aliases

--- install.zsh
+++ install.zsh
@@ -18,6 +18,7 @@ git submodule update
 ln -s	~/.config/editorconfig/editorconfig	~/.editorconfig
 ln -s	~/.config/git/gitignore				~/.gitignore
 ln -s	~/.config/git/gitconfig				~/.gitconfig
+ln -s	~/.config/homebrew/brew-aliases		~/.brew-aliases
 ln -s	~/.config/npm/npmrc					~/.npmrc
 ln -s	~/.config/zsh/zshrc					~/.zshrc
 ln -s	~/.config/bash/bash_profile			~/.bash_profile
